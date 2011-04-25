048a434f2ed57ff1a03dd374c603b2971f535869
Fabian Aussems <fabianaussems@gmail.com>
2011-04-25 20:42:56 +0200

    Changed clojure version number to 1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clj-native "0.9.1-SNAPSHOT"
   :description "Simplify usage of native libs from Clojure. Uses JNA."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [org.clojars.bagucode/jna "3.2.4"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.bagucode/jna "3.2.4"]])
