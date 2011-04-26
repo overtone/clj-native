14cd6fde990945be72e29aa717c15a726d47ea7f
Fabian Aussems <fabianaussems@gmail.com>
2011-04-26 14:16:11 +0200

    temporarly push clj-native into the overtone group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-native "0.9.1-SNAPSHOT"
+(defproject overtone/clj-native "0.9.2-SNAPSHOT"
   :description "Simplify usage of native libs from Clojure. Uses JNA."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
